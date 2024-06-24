316acf00e2c17a6d9d2fb73de32b930a650c828b
Scott Gardner <scott.gardner94@gmail.com>
2024-06-23 20:54:02 -0600

    implemented neat-python (NeuroEvolution of Augmenting Topologies)

--- main.py
+++ main.py
@@ -1,92 +1,203 @@
 # Example file showing a basic pygame "game loop"
+import os
+import sys
+import neat
+import neat.config
+import neat.population
 import pygame
-from settings import Player, Floor, BotPipe, TopPipe, width, height, bg
+from settings import birdToPipeCollision, Birds, Floor, BotPipe, TopPipe, width, height, bg
 import random
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-running = True
-running_time = 0
-released = True
-GameStarted = False
-topSpacing = 100
-botSpacing = -100
-score = 0
-pygame.font.init()
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
+generation = 0
+highscore = 0
 
-p1 = Player()
-floor = Floor()
-rngNUM = random.randint(botSpacing, topSpacing)
-botPipe = BotPipe(rngNUM)
-topPipe = TopPipe(rngNUM)
+def main(genomes, config):
+    pygame.init()
 
-all_spites_except_floor = pygame.sprite.Group()
-pipes = pygame.sprite.Group()
-collision_group = pygame.sprite.Group()
+    global generation, highscore
+    generation += 1
 
-pipes.add(botPipe, topPipe)
-collision_group.add(pipes, floor)
-all_spites_except_floor.add(p1, pipes)
+    screen = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+    running = True
+    topSpacing = 100
+    botSpacing = -100
+    score = 0
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if  event.key == pygame.K_SPACE:
-                released = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    pygame.font.init()
+    my_font = pygame.font.SysFont('Times New Roman', 30)
+
+    birds = []
+    ge = []
+    nets = []
+    distanceToPipes = []
+    textSurfaces = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Birds())
+        g.fitness = 0
+        ge.append(g)
+
+
+    floor1 = Floor(0)
+    floor2 = Floor(width)
+    floors = [floor1, floor2]
+
+    rngNUM = random.randint(botSpacing, topSpacing)
+    botPipe = BotPipe(rngNUM)
+    topPipe = TopPipe(rngNUM)
+
+    topPipes = [topPipe]
+    botPipes = [botPipe]
+
+    all_spites_except_floor = pygame.sprite.Group()
+    pipes = pygame.sprite.Group()
+    collision_group = pygame.sprite.Group()
+
+    pipes.add(botPipe, topPipe)
+    collision_group.add(pipes, floor1, floor2)
+    all_spites_except_floor.add(pipes)
+    for bird in birds:
+        all_spites_except_floor.add(bird)
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYUP:
+                if  event.key == pygame.K_SPACE:
+                    released = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    pygame.quit()
+                    quit()
 
-    if botPipe.rect[0] == width - 6 * 50:
-        rngNUM = random.randint(botSpacing, topSpacing)
-        botPipe = BotPipe(rngNUM)
-        topPipe = TopPipe(rngNUM)
-        pipes.add(botPipe, topPipe)
-        collision_group.add(botPipe, topPipe)
-        all_spites_except_floor.add(botPipe, topPipe)
+        if len(birds) == 0:
+            running =False
+            break
+        
+        if topPipes[len(topPipes)-1].rect[0] <= 970 and not topPipes[len(topPipes)-1].addPipe:
+            topPipes[len(topPipes)-1].addPipe = True
+            rngNUM = random.randint(botSpacing, topSpacing)
+            botPipe = BotPipe(rngNUM)
+            topPipe = TopPipe(rngNUM)
+            pipes.add(botPipe, topPipe)
+            collision_group.add(botPipe, topPipe)
+            all_spites_except_floor.add(botPipe, topPipe)
+            topPipes.append(topPipe)
+            botPipes.append(botPipe)
 
-    x = 0
-    while x <= width:
-        screen.blit(bg, (x,0))
-        x += bg.get_width()
+        x = 0
+        while x <= width:
+            screen.blit(bg, (x,0))
+            x += bg.get_width()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        if released:
-            GameStarted = p1.jump()
-            released = False
+        distanceToPipes.clear()
+        for pipe in topPipes:
+            distanceToPipes.append(pipe.rect[0] - birds[0].rect[0])
 
-    p1.move()
-    if GameStarted:
+        minPosDistance = sys.maxsize
+        for x, distance in enumerate(distanceToPipes):
+            if distance > 0 and distance < minPosDistance:
+                minPosDistance = distance
+                pipeindex = x
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            bird.draw_border(screen)
+            ge[x].fitness += 1/60
+
+            output = nets[x].activate((bird.rect[1], 
+                                       abs(bird.rect[1] - topPipes[pipeindex].rect[1] - topPipes[pipeindex].rect[3]),
+                                       abs(bird.rect[1] - botPipes[pipeindex].rect[1])))
+
+            if output[0] > 0.5 and bird.time > 0.2:
+                bird.jump()
+
         pipes.update()
 
-    for entity in all_spites_except_floor:
-        screen.blit(entity.image, entity.rect)
-    screen.blit(floor.image, floor.rect)
+        for _ in all_spites_except_floor:
+            screen.blit(_.image, _.rect)
+        for _ in floors:
+            _.move()
+            screen.blit(_.image, _.rect)
 
-    for pipe in pipes:
-        if pipe.rect[0] == 1280 - 189 * 6:
-            score += 1
-            break
+        for _ in topPipes:
+            if _.rect[0] + _.image.get_width() < 0:
+                topPipes.remove(_)
+                pipes.remove(_)
+            for bird in birds:
+                if bird.rect[0] > _.rect[0] + _.rect[2]:
+                    if not _.passed:
+                        score += 1
+                        if score > highscore:
+                            highscore = score
+                        for g in ge:
+                            g.fitness += 5
+                        _.passed = True
+                if _.passed:
+                    break
+            _.draw_border(screen)   
+        
+        for _ in botPipes:
+            if _.rect[0] + _.image.get_width() < 0:
+                botPipes.remove(_)
+                pipes.remove(_)
+            _.draw_border(screen)   
 
-    text_surface = my_font.render("Score: {}".format(score), False, (0,0,0))
-    screen.blit(text_surface, (0,0))
+        textSurfaces.clear()
+        textSurfaces.append(my_font.render("Score: {}".format(score), False, (0,0,0)))
+        textSurfaces.append(my_font.render("Highscore: {}".format(highscore), False, (0,0,0)))
+        textSurfaces.append(my_font.render("Generation: {}".format(generation), False, (0,0,0)))
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
 
-    if pygame.sprite.spritecollideany(p1, collision_group) or p1.rect[1] <= 0:
-        p1.kill()
-        running = False
+        for x, textSurface in enumerate(textSurfaces):
+            screen.blit(textSurface, (width-textSurface.get_width(),x * 30))
 
-    dt = clock.tick(60) / 1000 # limits FPS to 60
-    p1.time += dt
+        pygame.display.flip()
 
-pygame.quit()
+        for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if birdToPipeCollision(bird, pipe):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+                    bird.kill()
+
+        for x, bird in enumerate(birds):
+            if bird.rect[1] < 0 or bird.rect[1] + bird.rect[3] > floor1.top:
+                ge[x].fitness -= 1
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+                bird.kill()
+
+        dt = clock.tick(60) / 1000 # limits FPS to 60
+        for bird in birds:
+            bird.time += dt
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+    
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 100)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config_feedforward.txt")
+    run(config_path)
--- settings.py
+++ settings.py
@@ -1,26 +1,28 @@
 import pygame
 import os
+import random
 
 width = 1280
 height = 720
 
-class Player(pygame.sprite.Sprite):
+class Birds(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.red = random.randint(0,255)
+        self.green = random.randint(0,255)
+        self.blue = random.randint(0,255)
         self.time = 0
-        self.vel = 0
-        self.acc = 0
+        self.vel = -50
+        self.acc = 90
         self.image = pygame.image.load(os.path.join('images','bird2.png'))
         self.rect = self.image.get_rect()
         self.rect.move_ip(150, height/2-self.image.get_size()[0]/2)
 
     def jump(self):
         self.time = 0
-        self.vel = -50
-        self.acc = 90
         self.image = pygame.image.load(os.path.join('images','bird3.png'))
         self.image = pygame.transform.rotate(self.image, 18)
-        return True
+        self.rect = pygame.Rect(self.rect[0], self.rect[1], self.image.get_width(), self.image.get_height())
 
     def move(self):
         dx = self.vel*self.time + self.acc*self.time**2
@@ -28,44 +30,74 @@ class Player(pygame.sprite.Sprite):
         if dx > 0:
             self.image = pygame.image.load(os.path.join('images','bird1.png'))
             self.image = pygame.transform.rotate(self.image, -18)
+            self.rect = pygame.Rect(self.rect[0], self.rect[1], self.image.get_width(), self.image.get_height())
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.image)
+    
+    def draw_border(self, screen):
+        pygame.draw.rect(screen, (self.red, self.green, self.blue), self.rect, width=1)
 
 bg = pygame.image.load(os.path.join('images','bg.png'))
 bg = pygame.transform.scale(bg, (bg.get_width(), height))
 
 class Floor(pygame.sprite.Sprite):
-
-    def __init__(self):
+    def __init__(self, x):
         super().__init__()
         self.image = pygame.image.load(os.path.join('images','base.png'))
         self.image = pygame.transform.scale(self.image, (width, self.image.get_height()))
-        self.rect = self.image.get_rect()
-        self.rect.move_ip(0, height-self.image.get_height())
-        self.floorHeight = self.image.get_rect()[0]
+        self.top = height-self.image.get_height() 
+        self.rect = pygame.Rect(x, self.top, self.image.get_width(), self.image.get_height())
+
+    def move(self):
+        self.rect.move_ip(-6, 0)
+        if self.rect[0] <= -width:
+            self.rect.move_ip(2*width, 0)
 
 class Pipe(pygame.sprite.Sprite):
-    def __init__(self, rngNUM):
+    def __init__(self):
         super().__init__()
-        self.rngNUM = rngNUM
 
     def update(self):
         self.rect.move_ip(-6, 0)
 
-    def move(self):
-        self.rect.move_ip(0, self.rngNUM)
-
+    def move(self, rngNUM):
+        self.rect.move_ip(0, rngNUM)
+    
+    def draw_border(self, screen):
+        pygame.draw.rect(screen, (255,0,0), self.rect, width=1)
+        
+    def get_mask(self):
+        return pygame.mask.from_surface(self.image)
+    
 class BotPipe(Pipe):
     def __init__(self, rngNUM):
-        super().__init__(rngNUM)
+        super().__init__()
         self.image = pygame.image.load(os.path.join('images','pipe.png'))
         self.rect = self.image.get_rect()
         self.rect.move_ip(width, height - self.image.get_rect()[3])
-        super().move()
+        super().move(rngNUM)
 
-class TopPipe(Pipe, pygame.sprite.Sprite):
+class TopPipe(Pipe):
     def __init__(self, rngNUM):
-        super().__init__(rngNUM)
+        self.passed = False
+        self.addPipe = False
+        super().__init__()
         self.image = pygame.image.load(os.path.join('images','pipe.png'))
         self.rect = self.image.get_rect()
         self.rect.move_ip(width, -100)
-        self.image = pygame.transform.rotate(self.image, 180)
-        super().move()
+        self.image = pygame.transform.flip(self.image, False, True)
+        super().move(rngNUM)
+
+def birdToPipeCollision(bird, pipe):
+    bird_mask = bird.get_mask()
+    pipe_mask = pipe.get_mask()
+
+    offset = (pipe.rect[0] - bird.rect[0], pipe.rect[1] - bird.rect[1])
+
+    point = bird_mask.overlap(pipe_mask, offset)
+
+    if point:
+        return True
+    
+    return False
